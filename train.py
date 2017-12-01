9a4d8fb481cab30dcb2b5793f1bcd2528e8f6969
KEHANG <kehanghan@gmail.com>
2017-12-01 13:52:20 -0500

    create train method for training
    still need improvement, e.g., how to send batches into model fitting

--- train.py
+++ train.py
@@ -1,16 +1,32 @@
 import os
 
-from data import DataLoaderDisk
+from data import DataLoaderDisk, get_bag_of_words_embedding_matrix
+from model import vqa_model
 
-data_path = os.path.join('..', 'data')
-opt_data_train = {
-    'data_root': data_path,   # MODIFY PATH ACCORDINGLY
-    'fine_size': 224,
-    'word_embedding_length': 1024,
-    'randomize': False
-    }
+############################
+####### Run Training #######
+############################
+def train():
+    ### read training data and validation data
+    data_path = os.path.join('data')
+    opt_data_train = {
+        'data_root': data_path,   # MODIFY PATH ACCORDINGLY
+        'fine_size': 224,
+        'word_embedding_length': 1024,
+        'randomize': False
+        }
 
 
-loader_train = DataLoaderDisk(**opt_data_train)
+    loader_train = DataLoaderDisk(**opt_data_train)
 
-img_batch, que_batch, y_batch = loader_train.next_batch(100)
+    seq_length = 25
+    embedding_matrix = get_bag_of_words_embedding_matrix(loader_train.tokenizer.word_index)
+    model = vqa_model(embedding_matrix, seq_length, dropout_rate=0.5, num_classes=3131)
+    
+    img_batch, que_batch, y_batch = loader_train.next_batch(100)
+    hist = model.fit([img_batch, que_batch], y_batch, epochs=1, batch_size=1)
+
+
+train()
+
+
