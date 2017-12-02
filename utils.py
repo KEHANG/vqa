44c2bcabdbc1ec9166024cd0105ca813e002b33e
KEHANG <kehanghan@gmail.com>
2017-12-02 15:34:26 -0500

    add more arguments to argparse

--- utils.py
+++ utils.py
@@ -6,6 +6,12 @@ def parse_arguments():
 	parser.add_argument('-wf', '--weights_file', metavar='FILE', type=str, 
 		nargs=1, help='weights file for a pre-trained model')
 
+	parser.add_argument('-ebt', '--embedding_type', type=str, 
+		help='embedding type e.g., glove, bow')
+
+	parser.add_argument('-ebd', '--embedding_dim', type=int, 
+		help='embedding dimension')
+
 	return parser.parse_args()
 
 def log_to_file(msg):
