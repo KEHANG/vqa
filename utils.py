100ffa417f78b7e91ad8cc8f456904753638d174
KEHANG <kehanghan@gmail.com>
2017-12-02 14:06:00 -0500

    add method for parse arguments of script

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+import argparse
+
+def parse_arguments():
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-wf', '--weights_file', metavar='FILE', type=str, 
+		nargs=1, help='weights file for a pre-trained model')
+
+	return parser.parse_args()
 
 def log_to_file(msg):
     f = open('output.txt','a+')
