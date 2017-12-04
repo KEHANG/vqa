3fde2fab9151e83c8dea1f2184ce1266a4280d8f
KEHANG <kehanghan@gmail.com>
2017-12-04 12:16:08 -0500

    generalize image model creation
    so far we support vgg16, vgg19, resnet and xception

--- model.py
+++ model.py
@@ -6,6 +6,9 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM, Embedding, Merge
 from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
+from keras.applications.resnet50 import ResNet50
+from keras.applications.xception import Xception
 from keras.models import Model
 
 #######################
@@ -24,31 +27,40 @@ def Word2VecModel(embedding_matrix, seq_length, dropout_rate):
     model.add(Dense(1024, activation='tanh'))
     return model
 
-###########################
-## VGG model for image ##
-###########################
-def vgg_model():
-    print "Creating VGG image model..."
-    # Generate a model with all layers 
-    vgg16 = VGG16(include_top=True)
+##############################
+## CNN model for image ##
+##############################
+def image_model(model_name='vgg16'):
+    print "Creating {0} image model...".format(model_name)
+    # Generate a model with all layers
+    if model_name == 'vgg16':
+        pretrained_model = VGG16(include_top=True)
+    elif model_name == 'vgg19':
+        pretrained_model = VGG19(include_top=True)
+    elif model_name == 'resnet50':
+        pretrained_model = ResNet50(include_top=True)
+    elif model_name == 'xception':
+        pretrained_model = Xception(include_top=True)
+    else:
+        raise "Pre-trainied {0} not included yet.".format(model_name)
     # Make vgg16 model layers as non-trainable (freeze)
-    for layer in vgg16.layers:
+    for layer in pretrained_model.layers:
         layer.trainable = False
     # Add a layer where input is the output of the second last layer (before softmax)
-    new_layer = Dense(1024, input_dim=4096, activation='tanh')(vgg16.layers[-2].output)
+    new_layer = Dense(1024, activation='tanh')(pretrained_model.layers[-2].output)
     #Then create the corresponding model
-    model = Model(inputs=vgg16.input, outputs=new_layer)
+    model = Model(inputs=pretrained_model.input, outputs=new_layer)
     return model
 
 #########################
 ## Aggregate VQA model ##
 #########################
-def vqa_model(embedding_matrix, seq_length, dropout_rate, num_classes):
-    vgg = vgg_model()
+def vqa_model(image_model_name, embedding_matrix, seq_length, dropout_rate, num_classes):
+    img_model = image_model(model_name=image_model_name)
     lstm_model = Word2VecModel(embedding_matrix, seq_length, dropout_rate)
     print "Merging final model..."
     fc_model = Sequential()
-    fc_model.add(Merge([vgg, lstm_model], mode='mul'))
+    fc_model.add(Merge([img_model, lstm_model], mode='mul'))
     fc_model.add(Dropout(dropout_rate))
     fc_model.add(Dense(1000, activation='tanh'))
     fc_model.add(Dropout(dropout_rate))
